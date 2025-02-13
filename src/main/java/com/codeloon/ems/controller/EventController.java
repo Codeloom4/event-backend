0e348ec7389f751dc2772daf5ad5142a62c3f212
akash_a <akash_a@epiclanka.net>
2025-02-13 23:22:33 +0530

    event commonresponsebean

--- src/main/java/com/codeloon/ems/controller/EventController.java
+++ src/main/java/com/codeloon/ems/controller/EventController.java
@@ -6,12 +6,14 @@ import com.codeloon.ems.model.EventBean;
 import com.codeloon.ems.service.EventService;
 import com.codeloon.ems.util.ResponseBean;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/ems/events")
 @RequiredArgsConstructor
@@ -19,7 +21,7 @@ public class EventController {
 
     private final EventService eventService;
 
-    //TODO open same endpoint in Home controller.
+
     @GetMapping
     public ResponseEntity<List<EventBean>> getAllEvents() {
         return ResponseEntity.ok(eventService.getAllEvents());
@@ -28,18 +30,61 @@ public class EventController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseBean> getEventById(@PathVariable Long id) {
-        return ResponseEntity.ok(eventService.findEventById(id));
+    public ResponseEntity<?> getEventById(@PathVariable Long id) {
+        ResponseEntity<?> responseEntity;
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        ResponseBean responseBean = new ResponseBean();
+        try {
+            responseBean = eventService.findEventById(id);
+            httpStatus = HttpStatus.OK;
+        } catch (Exception ex) {
+            log.error("Error occurred while getting Event Type by ID.{} ", ex.getMessage());
+        } finally {
+            responseEntity = new ResponseEntity<>(responseBean, httpStatus);
+        }
+        return responseEntity;
+
     }
 
     @PostMapping
-    public ResponseEntity<ResponseBean> createEvent(@RequestBody EventBean eventBean) {
-        return ResponseEntity.ok(eventService.createEvent(eventBean));
+    public ResponseEntity<?> createEvent(@RequestBody EventBean eventBean) {
+        ResponseEntity<?> responseEntity;
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        ResponseBean responseBean = new ResponseBean();
+                try {
+            responseBean = eventService.createEvent(eventBean);
+            httpStatus = HttpStatus.CREATED;
+        } catch (Exception ex) {
+            log.error("Error occurred while saving new Event Type.{} ", ex.getMessage());
+        } finally {
+            responseEntity = new ResponseEntity<>(responseBean, httpStatus);
+        }
+        return responseEntity;
+
     }
 
+
+
+
+
+
+
+
     @PutMapping("/{id}")
-    public ResponseEntity<ResponseBean> updateEvent(@PathVariable Long id, @RequestBody EventBean eventBean) {
-        return ResponseEntity.ok(eventService.updateEvent(id, eventBean));
+    public ResponseEntity<?> updateEvent(@PathVariable Long id, @RequestBody EventBean eventBean) {
+        ResponseEntity<?> responseEntity;
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        ResponseBean responseBean = new ResponseBean();
+        try {
+            responseBean = eventService.updateEvent(id, eventBean);
+            httpStatus = HttpStatus.CREATED;
+        } catch (Exception ex) {
+            log.error("Error occurred while saving new Event Type.{} ", ex.getMessage());
+        } finally {
+            responseEntity = new ResponseEntity<>(responseBean, httpStatus);
+        }
+        return responseEntity;
+
     }
 
     @DeleteMapping("/{id}")
