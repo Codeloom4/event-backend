f54fe1f7fb19bff45bf4240c96311aca365e8a42
akash_a <akash_a@epiclanka.net>
2025-02-13 20:31:24 +0530

    payment mgt plus eventmgt remove event name

---
+++ src/main/java/com/codeloon/ems/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.codeloon.ems.repository;
+
+import com.codeloon.ems.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
+
