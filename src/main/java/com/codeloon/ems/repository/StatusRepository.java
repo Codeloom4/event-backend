f9907f2b1919e96d08ef7419cdb2c180a0520be9
SandaliR329 <sandalirj@gmail.com>
2025-03-16 16:04:43 +0530

    Commit create order

---
+++ src/main/java/com/codeloon/ems/repository/StatusRepository.java
@@ -0,0 +1,9 @@
+package com.codeloon.ems.repository;
+
+import com.codeloon.ems.entity.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusRepository extends JpaRepository<Status, String> {
+
+
+}
