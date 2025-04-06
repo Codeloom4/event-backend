e270d284908b20559ce1d2b91f70606f5bd7366c
damithrm <damithrmdr@gmail.com>
2025-04-06 19:51:48 +0530

    Scheduling Email

--- src/main/java/com/codeloon/ems/repository/OrderRequestRepository.java
+++ src/main/java/com/codeloon/ems/repository/OrderRequestRepository.java
@@ -3,11 +3,16 @@ package com.codeloon.ems.repository;
 import com.codeloon.ems.entity.Inventory;
 import com.codeloon.ems.entity.InventoryItem;
 import com.codeloon.ems.entity.OrderRequest;
+import com.codeloon.ems.entity.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface OrderRequestRepository extends JpaRepository<OrderRequest,String> {
@@ -21,4 +26,6 @@ public interface OrderRequestRepository extends JpaRepository<OrderRequest,Strin
 
     Page<OrderRequest> findAllByOrderStatus(String status, Pageable pageable);
 
+
+    List<OrderRequest> findByRequestedDateBetweenAndOrderStatus(LocalDateTime startOfDay, LocalDateTime endOfDay, Status status);
 }
--- src/main/java/com/codeloon/ems/service/OrderReminderService.java
+++ src/main/java/com/codeloon/ems/service/OrderReminderService.java
@@ -1,5 +1,11 @@
 package com.codeloon.ems.service;
 
+import com.codeloon.ems.entity.OrderRequest;
+import com.codeloon.ems.entity.Status;
+import com.codeloon.ems.entity.User;
+import com.codeloon.ems.repository.OrderRequestRepository;
+import com.codeloon.ems.repository.StatusRepository;
+import com.codeloon.ems.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,13 +13,22 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderReminderService {
 
-//    @Autowired
-//    private OrderRepository orderRepository;
+    @Autowired
+    private OrderRequestRepository OrderRequestRepository;
+
+    @Autowired
+    private StatusRepository statusRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private JavaMailSender mailSender;
@@ -21,23 +36,28 @@ public class OrderReminderService {
     @Scheduled(cron = "0 0 0 * * ?") // Runs every day at midnight
 //    @Scheduled(cron = "0 * * * * ?") // Runs every minute
     public void sendReminderEmails() {
-//        LocalDate oneYearAgo = LocalDate.now().minusYears(1);
-//        List<Order> orders = orderRepository.findByOrderDate(oneYearAgo);
+        LocalDate oneYearAgoDate = LocalDate.now().minusYears(1);
+        LocalDateTime startOfDay = oneYearAgoDate.atStartOfDay();               // 2024-04-06T00:00
+        LocalDateTime endOfDay = oneYearAgoDate.atTime(LocalTime.MAX);          // 2024-04-06T23:59:59.999999999
 
-//        for (Order order : orders) {
-//            sendEmail(order.getUser().getEmail(), order);
-//        }
-//        sendEmail(order.getUser().getEmail(), order);
-        sendEmail();
+        Optional<Status> paymentApprovedStatus = statusRepository.findById("PAYMENT_APPROVED");
+
+        List<OrderRequest> orders = OrderRequestRepository
+                .findByRequestedDateBetweenAndOrderStatus(startOfDay, endOfDay, paymentApprovedStatus.get());
+
+        for (OrderRequest order : orders) {
+            Optional<User> userDetails = userRepository.findByUsername(order.getCustomerUsername());
+            userDetails.ifPresent(user -> sendEmail(user.getEmail(), order));
+        }
+
     }
 
-//    private void sendEmail(String toEmail, Order order) {
-    private void sendEmail() {
+    private void sendEmail(String toEmail, OrderRequest order) {
         SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(toEmail);
-        message.setTo("damithrmdr@gmail.com");
-        message.setSubject("Event Order Anniversary Reminder");
-        message.setText("Dear Customer, It's been a year since your order. We appreciate your support!");
+        message.setTo(toEmail);
+        message.setSubject("Last Year Order Reminder");
+        String text = "Dear Customer, It's been a year since your order. " + order.getCustomerNote() + ", We appreciate your support!";
+        message.setText(text);
 
         mailSender.send(message);
     }
